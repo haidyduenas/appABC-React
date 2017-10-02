57cd5bea0d9d13230a88f568ad17a269f1218809
haidy <haidy>
2017-10-01 23:49:20 -0500

    preguntas estaticas

--- app.jsx
+++ app.jsx
@@ -2,31 +2,31 @@ let items = [
     { 
         question: "La pelicula más taquillera de Pixar", 
         options : ["El Rey Leon", "Los Increibles", "Frozen"] ,
-        imagen:"assets/img/fondo.png",
+        image:"assets/img/fondo.png",
         answer : 2
     },
     { 
         question: "Pelicula animada más premiada?", 
         options:  ["Buscando a Nemo", "El rey Leon", "UP"] ,
-        imagen :"assets/img/sou.png",
+        image :"assets/img/sou.png",
         answer : 1
     },
     { 
         question: "La pelicula ganadora del Oscar a la mejor animacion 2004?", 
         options:["Enredados", "Los Increibles", "Toy Story"], 
-        imagen:"assets/img/in.png",
+        image:"assets/img/in.png",
         answer : 1
     },
     { 
         question: "Largometraje con mas nominaciones al Oscar?", 
         options : ["Wall-E", "Monster Inc","Dory"],
-        imagen:"assets/img/pumba.png",
+        image:"assets/img/pumba.png",
         answer : 0 
     },
     { 
         question: "Record al debút más grande para un pelicula animada?", 
         options :["Bichos", "Bolt","Ratatouille"],
-        imagen :"assets/img/fro.png",     
+        image :"assets/img/fro.png",     
         answer : 2
     }
  ];
@@ -46,13 +46,8 @@ class Quiz extends React.Component {
             <h1 className="text-center"> 
                 {items[0].question} 
             </h1>
-          </div>
+        </div>
         )
-       
-        //eliminar todo en el área de preguntas
-            questionArea.innerHTML = "";
-        //agregar todas las respuestas posibles el área de respuesta
-            questionArea.innerHTML = question;
     }
 
     
@@ -62,7 +57,7 @@ class Quiz extends React.Component {
             <header>
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 img-question">
-                    <img className="img-responsive img-questions"/>
+                    <img className="img-responsive img-questions" src={items[0].image}/>
                 </div>
             </div>
             </header>
@@ -80,18 +75,18 @@ class Quiz extends React.Component {
                 </div>
                 <div className="btn-cont answers">
                     <div className="col-lg-4 col-sm-4 col-xs-12">
-                        <button className="btn btn-huge" id="opcion1" type="button" name="button">
-                            <span className="letter ">A</span>Avianca
+                        <button className="btn btn-huge" type="button" name="button">
+                            <span className="letter">A</span>{items[0].options[0]}
                         </button>
                     </div>
                     <div className="col-lg-4 col-sm-4 col-xs-12">
                         <button className="btn btn-huge" type="button" name="button">
-                            <span className="letter">B</span>KLM
+                            <span className="letter">B</span>{items[0].options[1]}
                         </button>
                     </div>
                     <div className="col-lg-4 col-sm-4 col-xs-12">
                         <button className="btn btn-huge" type="button" name="button">
-                            <span className="letter">C</span>Qantas
+                            <span className="letter">C</span>{items[0].options[2]}
                         </button>
                     </div>
                 </div>
